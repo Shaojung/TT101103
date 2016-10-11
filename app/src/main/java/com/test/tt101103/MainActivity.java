9c62df774cffc78206de06123ad12f5676689b11
Shaojung <shaojung@gmail.com>
2016-10-11 15:49:54 +0800

    put JSON in listview

--- app/src/main/java/com/test/tt101103/MainActivity.java
+++ app/src/main/java/com/test/tt101103/MainActivity.java
@@ -3,6 +3,8 @@ package com.test.tt101103;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -14,14 +16,19 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
+public class MainActivity extends AppCompatActivity {
+    ArrayList<Map<String, String>> data;
+    ListView lv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        data = new ArrayList<>();
+        lv =(ListView) findViewById(R.id.listView);
         RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
         final StringRequest request = new StringRequest("http://data.ntpc.gov.tw/od/data/api/BF90FA7E-C358-4CDA-B579-B6C84ADC96A1?$format=json",
                 new Response.Listener<String>() {
@@ -34,9 +41,22 @@ public class MainActivity extends AppCompatActivity {
                             for (int i=0;i<array.length();i++)
                             {
                                 JSONObject obj = array.getJSONObject(i);
-                                String str = obj.getString("district");
-                                Log.d("DATA", str);
+
+                                Map m = new HashMap();
+                                m.put("district", obj.getString("district"));
+                                m.put("address", obj.getString("address"));
+                                data.add(m);
                             }
+
+                            SimpleAdapter adapter = new SimpleAdapter(
+                                        MainActivity.this,
+                                        data,
+                                        android.R.layout.simple_list_item_2,
+                                        new String[] {"district", "address"},
+                                        new int[] {android.R.id.text1, android.R.id.text2}
+                            );
+                            lv.setAdapter(adapter);
+
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
